08f5922ff34b71e230e285793d12eb59c9ab8265
deltinix <78965446+deltinix@users.noreply.github.com>
2021-02-27 13:26:36 -0300

    Exercício da Aula 2

---
+++ Exercicio Aula 2.py
@@ -0,0 +1,26 @@
+'''
+EXERCÍCIO PROPOSTO
+
+Faça um formulário que pergunte o Nome, CPF, Endereço, Idade, Altura e Telefone
+e imprima isso em um relatório organizado!
+
+'''
+
+nome =     input('Nome.......: ')
+cpf =      input('CPF........: ')
+endereco = input('Endereço...: ')
+idade    = input('Idade......: ')
+altura   = input('Altura.....: ')
+telefone = input('Telefone...: ')
+
+print('\n')
+print('===================================')
+print('====== RELATÓRIO DE CADASTRO ======')
+print('===================================')
+print('Seu nome é',nome)
+print('Seu CPF é',cpf)
+print('Seu endereço é',endereco)
+print('Sua idade é de',idade, 'anos')
+print('Sua altura é de',altura, 'metros')
+print('Seu telefone é',telefone)
+print('\nRelatório Finalizado...\n')
